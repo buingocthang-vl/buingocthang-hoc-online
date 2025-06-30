a3da19eaadc0fc71594db0871277d58a2ea79c3e
buingocthang-vl <thangbn9588@gmail.com>
2025-06-30 23:32:29 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,14 @@
+const questions = [
+  {
+    question: "Giá trị của $\\int_0^1 x^2 \\,dx$ là bao nhiêu?",
+    options: ["A. 1/3", "B. 1/2", "C. 1", "D. 2"],
+    answer: "A"
+  },
+  {
+    question: "Hình dưới biểu diễn hiện tượng gì?<br><img src='https://i.imgur.com/3x9Q6xE.png' width='300'/>",
+    options: ["A. Giao thoa", "B. Khúc xạ", "C. Phản xạ", "D. Nhiễu xạ"],
+    answer: "A"
+  }
+];
+
+export default questions;
